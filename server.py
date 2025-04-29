043fa2dfcc403929738f297e6143d94b7612a364
Uchiha-Sora <janakiraman0413@gmail.com>
2025-04-29 12:53:40 +0530

    Updated file 1

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 from fastapi import FastAPI
 from io import BytesIO
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
+# Your ESP32-CAM snapshot URL
 cam_url = "http://10.18.114.67/capture"
 
 @app.get("/")
@@ -14,10 +16,15 @@ def read_root():
 
 @app.get("/frame")
 def get_frame():
+    # Fetch snapshot
     response = requests.get(cam_url)
     if response.status_code == 200:
-        img_array = np.asarray(bytearray(response.content), dtype = np.unit8)
+        # Decode image
+        img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
         frame = cv2.imdecode(img_array, -1)
-        _, img_encoded = cv2.imencode('jpg', frame)
-        return StreamingResponse(ByteIO(img_encoded.tobytes()), media_type = "image/jpeg")
-    return {"error":"Couldn't Fetch Frame"}
+        _, img_encoded = cv2.imencode('.jpg', frame)
+
+        # Return JPEG image as a streaming response
+        return StreamingResponse(BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
+    
+    return {"error": "Couldn't Fetch Frame"}
