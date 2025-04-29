400d2531d8b8cbacf50bceca6e714b053769d992
Uchiha-Sora <janakiraman0413@gmail.com>
2025-04-29 12:05:37 +0530

    ESP32 Cam Checking

---
+++ .ipynb_checkpoints/server-checkpoint.py
@@ -0,0 +1,23 @@
+import requests
+import cv2
+import numpy as np
+from fastapi import FastAPI
+from io import BytesIO
+
+app = FastAPI()
+
+cam_url = "http://10.18.114.67/capture"
+
+@app.get("/")
+def read_root():
+    return {"message": "ESP32 Cam with Server Testing"}
+
+@app.get("/frame")
+def get_frame():
+    response = request.get(cam_url)
+    if response.status_code == 200:
+        img_array = np.asarray(bytearray(response.content), dtype = np.unit8)
+        frame = cv2.imdecode(img_array, -1)
+        _, img_encoded = cv2.imencode('jpg', frame)
+        return StreamingResponse(ByteIO(img_encoded.tobytes()), media_type = "image/jpeg")
+    return {"error":"Couldn't Fetch Frame"}
